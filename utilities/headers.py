4df0d88e7d3a485fc73a3a26cfa7c9b8d700f772
Lynne Merchant <lmerchant@ucsd.edu>
2019-05-28 11:32:22 -0700

    Round latitude and longitude to 5 places

--- utilities/headers.py
+++ utilities/headers.py
@@ -35,13 +35,22 @@ def create_metadata_header(data_set):
     # Since metadata same in all rows, pick first row to get values from
     first_row = data_set.iloc[0]
 
+
+    # round latitude and longitude because float of 5 decimal places
+    # is leading to too many decimal places
+    # When pandas rounds, it rounds to max number of decimal
+    # places in the column. So 125.458 stays as is and no
+    # 0 padding is added at the end
+    latitude = first_row['LATITUDE'].round(5)
+    longitude = first_row['LONGITUDE'].round(5)
+
     metadata_header.append('EXPOCODE = ' + first_row['EXPOCODE'])
     metadata_header.append('STNBR = ' + first_row['STATION'])
     metadata_header.append('CASTNO = ' + str(first_row['CASTNO']))
     metadata_header.append('DATE = ' + first_row['DATE'])
     metadata_header.append('TIME = ' + first_row['TIME'])
-    metadata_header.append('LATITUDE = ' + str(first_row['LATITUDE']))
-    metadata_header.append('LONGITUDE = ' + str(first_row['LONGITUDE']))
+    metadata_header.append('LATITUDE = ' + str(latitude))
+    metadata_header.append('LONGITUDE = ' + str(longitude))
 
     # NUMBER_HEADERS counts all metadata values plus itself
     number_headers = len(metadata_header) + 1
