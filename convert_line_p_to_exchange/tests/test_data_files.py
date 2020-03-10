53b80fa74bc88eb40a1d33200b5c1495075508a1
Lynne Merchant <lmerchant@ucsd.edu>
2020-03-10 09:52:19 -0700

    New tests folder in source folder

---
+++ convert_line_p_to_exchange/tests/test_data_files.py
@@ -0,0 +1,216 @@
+import pytest
+import pandas as pd
+
+from utilities.data_files import *
+import utilities.process_raw_data
+
+
+def test_create_start_end_lines(monkeypatch):
+
+    NOW_TIME = datetime.datetime(2020, 12, 25, 17, 5, 55)
+
+    class mydatetime:
+        def now(self):
+            return NOW_TIME
+
+
+    monkeypatch.setattr(datetime, 'datetime', mydatetime())
+
+    expected_start_line = start_line = "CTD,20201225CCHSIOLM"
+    expected_end_line = 'END_DATA'
+
+
+    real_start_line, real_end_line = create_start_end_lines()
+
+    assert real_start_line == expected_start_line
+
+    assert real_end_line == expected_end_line
+    
+
+@pytest.fixture
+def station_variation_sets():
+
+    # STATION = 'P4'
+    column_names = ['File Name', 'Zone', 'DATE', 'TIME', 'EVENT', 'LATITUDE', 'LONGITUDE', 'EXPOCODE', 'STATION', 'CASTNO', 'INS:LOCATION', 'CTDPRS']
+
+    data = [['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'P4', '10', 'Mid-ship', '2'], ['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'P4', '10', 'Mid-ship', '2']]
+
+    testing_df1 = pd.DataFrame(data, columns = column_names)
+
+    # STATION = 'P1/B8'
+    column_names = ['File Name', 'Zone', 'DATE', 'TIME', 'EVENT', 'LATITUDE', 'LONGITUDE', 'EXPOCODE', 'STATION', 'CASTNO', 'INS:LOCATION', 'CTDPRS']
+
+    data = [['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'P1/B8', '100', 'Mid-ship', '2'], ['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'P1/B8', '100', 'Mid-ship', '2']]
+
+    testing_df2 = pd.DataFrame(data, columns = column_names)
+
+    # STATION = 'PA-001'
+    column_names = ['File Name', 'Zone', 'DATE', 'TIME', 'EVENT', 'LATITUDE', 'LONGITUDE', 'EXPOCODE', 'STATION', 'CASTNO', 'INS:LOCATION', 'CTDPRS']
+
+    data = [['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'PA-001', '1', 'Mid-ship', '2'], ['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'PA-001', '1', 'Mid-ship', '2']]
+
+    testing_df3 = pd.DataFrame(data, columns = column_names)
+
+    return (testing_df1, testing_df2, testing_df3)
+
+
+def test_get_ctd_filename(station_variation_sets):
+
+    testing_df1, testing_df2, testing_df3 = station_variation_sets
+
+    directory = 'data/<expocode>_ct1/'
+
+    expected_filename1 = 'data/<expocode>_ct1/<expocode>_000P4_00010_ct1.csv'
+
+    real_filename1 = get_ctd_filename(directory, testing_df1)
+
+    assert real_filename1 == expected_filename1
+
+
+    expected_filename2 = 'data/<expocode>_ct1/<expocode>_P1-B8_00100_ct1.csv'
+
+    real_filename2 = get_ctd_filename(directory, testing_df2)
+
+    assert real_filename2 == expected_filename2
+
+
+    expected_filename3 = 'data/<expocode>_ct1/<expocode>_PA001_00001_ct1.csv'
+
+    real_filename3 = get_ctd_filename(directory, testing_df3)
+
+    assert real_filename3 == expected_filename3
+
+
+def test_get_individual_raw_file(monkeypatch):
+
+    column_names = ['File Name', 'Zone', 'DATE', 'TIME', 'EVENT', 'LATITUDE', 'LONGITUDE', 'EXPOCODE', 'STATION', 'CASTNO', 'INS:LOCATION', 'CTDPRS']
+
+    data = [['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'P4', '10', 'Mid-ship', '2'], ['2017-01-0017.ctd', 'UTC', '20170207', '1640', '18', '48.6485', '-126.667', '<expocode>', 'P4', '10', 'Mid-ship', '2']]
+
+    testing_df1 = pd.DataFrame(data, columns = column_names)
+
+    def mock_get_cruise_list():
+        return [('<line_p_year>', '<line_p_id>', '<expocode>')]
+
+    monkeypatch.setattr(utilities.process_raw_data, 'get_cruise_list', mock_get_cruise_list)
+
+    def mock_get_ids(expocode, cruise_list):
+        return '<line_p_year>', '<line_p_id>'
+
+    monkeypatch.setattr(utilities.data_files,'get_line_p_cruise_id', mock_get_ids)
+
+    expected_url = 'https://www.waterproperties.ca/linep/<line_p_year>-<line_p_id>/donneesctddata/<line_p_year>-<line_p_id>-0018.ctd'
+
+    expocode = '<expocode>'
+
+    real_url = get_individual_raw_file(expocode, testing_df1)
+
+    assert real_url == expected_url
+
+
+def test_get_individual_raw_file_header(monkeypatch):
+
+    testing_header = ["*2014/12/02 11:39:47.13",
+        "*IOS HEADER VERSION 1.10 2011/10/26 2011/10/26",
+        "",
+        "*FILE",
+        "    START TIME          : UTC 2007/02/09 10:20:43.000",
+        "!--1--- ---2---- ---3---- ---4---- --5-- --6--- --7-- ---8---",
+        "!Pressu Temperat Salinity Sigma-t: Trans Oxygen Oxyge Fluores",
+        "!re:CTD ure:CTD  :CTD     CTD      missi :Disso n:Dis cence: ",
+        "!                                  vity: lved:  solve CTD:   ",
+        "!                                  CTD   CTD:   d:CTD Seapoin",
+        "!                                        Volume :Mass t      ",
+        "!------ -------- -------- -------- ----- ------ ----- -------",
+        "*END OF HEADER",
+        "    4.3   8.4168  31.9619  24.8539  45.3   6.43 280.1   0.619"]
+
+    expected_header = ["#*2014/12/02 11:39:47.13",
+        "#*IOS HEADER VERSION 1.10 2011/10/26 2011/10/26",
+        "#",
+        "#*FILE",
+        "#    START TIME          : UTC 2007/02/09 10:20:43.000",
+        "#!--1--- ---2---- ---3---- ---4---- --5-- --6--- --7-- ---8---",
+        "#!Pressu Temperat Salinity Sigma-t: Trans Oxygen Oxyge Fluores",
+        "#!re:CTD ure:CTD  :CTD     CTD      missi :Disso n:Dis cence: ",
+        "#!                                  vity: lved:  solve CTD:   ",
+        "#!                                  CTD   CTD:   d:CTD Seapoin",
+        "#!                                        Volume :Mass t      ",
+        "#!------ -------- -------- -------- ----- ------ ----- -------",
+        "#*END OF HEADER"]
+
+
+
+    def mock_get_file_text(url):
+        return testing_header
+
+    monkeypatch.setattr(utilities.data_files, 'get_file_text', mock_get_file_text)
+
+    url = 'https://...'
+
+    real_header = get_individual_raw_file_header(url)
+
+    assert real_header == expected_header
+
+
+@pytest.mark.parametrize("EXCEPTION_FLAG", [False, True])
+def test_choose_raw_file_header(monkeypatch, EXCEPTION_FLAG):
+
+    # Test for cases of url found and not found
+
+    def mock_get_file_header(url):
+        if EXCEPTION_FLAG:
+            raise Exception('url not found')
+        else:
+            return ['# Comment Header Line 1', '# Comment Header Line 2']
+
+    monkeypatch.setattr(utilities.data_files, 'get_individual_raw_file_header', mock_get_file_header)
+
+    # url either of individual file by event number, a corrected individ file, or no individ file found
+    url1 = "https://individual_file"
+    url2 = "https://www.waterproperties.ca/linep/2009-09/donneesctddata/2009-09-0014.ctd"  
+    url3 = "https://individual_file_not_found"  
+
+    comment_header = ["comment"]
+    ctd_filename = "ctd_filename"
+
+
+    # Case 1: url is of individual file by event number
+    real_header = choose_raw_file_header(url1, comment_header, ctd_filename)
+
+    if EXCEPTION_FLAG:
+        expected_header = ["comment"]
+    else:
+        expected_header = ['# Comment Header Line 1', '# Comment Header Line 2']
+
+    assert real_header == expected_header
+
+
+    # Case 2: url is a correction of individual file by event number
+    #         and is searched for only if an exception occurs
+    try:
+        real_header = choose_raw_file_header(url2, comment_header, ctd_filename)
+        expected_header = ['# Comment Header Line 1', '# Comment Header Line 2']
+    except:
+        real_header = ['# Comment Header Line 1', '# Comment Header Line 2']
+        expected_header = ['# Comment Header Line 1', '# Comment Header Line 2']
+
+    assert real_header == expected_header
+
+
+    # Case 3: url of an individual file by event number not found
+    # and is searched for only if an exception occurs
+
+    real_header = choose_raw_file_header(url3, comment_header, ctd_filename)
+
+    if EXCEPTION_FLAG:
+        expected_header = ["comment"]
+    else:
+        expected_header = ['# Comment Header Line 1', '# Comment Header Line 2']
+
+    assert real_header == expected_header
+
+
+
+
+
---
+++ convert_line_p_to_exchange/tests/test_parameters.py
@@ -0,0 +1,130 @@
+import pytest
+import pandas as pd
+
+from utilities.parameters import *
+
+
+
+@pytest.fixture()
+def df_column_name_sets():
+
+    # Mapping of the original column names to WHP names. Sets were extracted from real files
+    # to get all combinations of parameter long names that exist. 
+
+    column_names = ['File Name','Pressure:CTD [dbar]','Temperature:CTD [deg_C_(ITS90)]','Salinity:CTD [PSS-78]','Sigma-t:CTD [kg/m^3]','Transmissivity:CTD [*/m]','Oxygen:Dissolved:CTD:Volume [ml/l]','Oxygen:Dissolved:CTD:Mass [µmol/kg]','Fluorescence:CTD:Seapoint [mg/m^3]','PAR:CTD [µE/m^2/sec]']
+    data = [['2007-13-0003.ctd',3.2,'11.0119','31.4992','24.0747','18.4','6.98','304.4','6.05','']]
+    testing_df_set1 = pd.DataFrame(data, columns=column_names)
+
+    expected_params_set1 =[                             
+    {'whpname' : 'CTDPRS' , 'longname':'Pressure:CTD [dbar]', 'units' : 'DBAR'},
+    {'whpname' : 'CTDTMP' , 'longname':'Temperature:CTD [deg_C_(ITS90)]', 'units' : 'ITS-90'},
+    {'whpname' : 'CTDSAL' , 'longname':'Salinity:CTD [PSS-78]', 'units' : 'PSS-78'},
+    {'whpname' : 'CTDOXY' , 'longname':'Oxygen:Dissolved:CTD:Mass [µmol/kg]', 'units' : 'UMOL/KG'},   
+    {'whpname' : 'CTDBEAMCP' , 'longname':'Transmissivity:CTD [*/m]', 'units' : '/METER'},
+    {'whpname' : 'CTDFLUOR' , 'longname':'Fluorescence:CTD:Seapoint [mg/m^3]', 'units' : 'MG/M^3'},   
+    {'whpname' : 'PAR' , 'longname':'PAR:CTD [µE/m^2/sec]', 'units' : 'UE/m^2/sec'}     
+    ] 
+
+    column_names = ['File Name','Pressure:CTD [dbar]','Temperature:CTD [deg_C_(ITS90)]','Salinity:Practical:CTD [PSS-78]','Sigma-t:CTD [kg/m^3]','Transmissivity:CTD [*/m]','Oxygen:Dissolved:CTD:Volume [ml/l]','Oxygen:Dissolved:CTD:Mass [µmol/kg]','Fluorescence:CTD [mg/m^3]','PAR:CTD [µE/m^2/sec]']
+    data = [['2007-15-0003.ctd',4.0,'11.4455','31.1325','23.7134','33.8','5.24','228.7','2.242','0.2']]
+    testing_df_set2 = pd.DataFrame(data, columns=column_names)
+
+    expected_params_set2 =[                             
+    {'whpname' : 'CTDPRS' , 'longname':'Pressure:CTD [dbar]', 'units' : 'DBAR'},
+    {'whpname' : 'CTDTMP' , 'longname':'Temperature:CTD [deg_C_(ITS90)]', 'units' : 'ITS-90'},
+    {'whpname' : 'CTDSAL' , 'longname':'Salinity:Practical:CTD [PSS-78]', 'units' : 'PSS-78'},
+    {'whpname' : 'CTDOXY' , 'longname':'Oxygen:Dissolved:CTD:Mass [µmol/kg]', 'units' : 'UMOL/KG'},   
+    {'whpname' : 'CTDBEAMCP' , 'longname':'Transmissivity:CTD [*/m]', 'units' : '/METER'},
+    {'whpname' : 'CTDFLUOR', 'longname': 'Fluorescence:CTD [mg/m^3]', 'units' : 'MG/M^3'},
+    {'whpname' : 'PAR' , 'longname':'PAR:CTD [µE/m^2/sec]', 'units' : 'UE/m^2/sec'}     
+    ] 
+
+
+    column_names = ['File Name','Pressure:CTD [dbar]','Temperature:CTD [deg_C_(ITS90)]','Salinity:CTD [PSS-78]','Sigma-t:CTD [kg/m^3]','Transmissivity:CTD [%/m]','Oxygen:Dissolved:CTD:Volume [ml/l]','Oxygen:Dissolved:CTD:Mass [µmol/kg]','Fluorescence:CTD:Seapoint [mg/m^3]','PAR:CTD [µE/m^2/sec]']
+    data = [['2013-17-0002.ctd',3.0,'12.0342','30.2748','22.9419','23.9','6.28','274.1','2.002','248.8']] 
+    testing_df_set3 = pd.DataFrame(data, columns=column_names)
+
+    expected_params_set3 =[                             
+    {'whpname' : 'CTDPRS' , 'longname':'Pressure:CTD [dbar]', 'units' : 'DBAR'},
+    {'whpname' : 'CTDTMP' , 'longname':'Temperature:CTD [deg_C_(ITS90)]', 'units' : 'ITS-90'},
+    {'whpname' : 'CTDSAL' , 'longname':'Salinity:CTD [PSS-78]', 'units' : 'PSS-78'},
+    {'whpname' : 'CTDOXY' , 'longname':'Oxygen:Dissolved:CTD:Mass [µmol/kg]', 'units' : 'UMOL/KG'},   
+    {'whpname' : 'CTDBEAMCP' , 'longname':'Transmissivity:CTD [%/m]', 'units' : '/METER'},
+    {'whpname' : 'CTDFLUOR' , 'longname':'Fluorescence:CTD:Seapoint [mg/m^3]', 'units' : 'MG/M^3'},   
+    {'whpname' : 'PAR' , 'longname':'PAR:CTD [µE/m^2/sec]', 'units' : 'UE/m^2/sec'}     
+    ] 
+
+
+    column_names = ['File Name','Pressure:CTD [dbar]','Temperature:CTD [deg_C_(ITS90)]','Salinity:CTD [PSS-78]','Sigma-t:CTD [kg/m^3]','Transmissivity:CTD [*/m]','Oxygen:Dissolved:CTD:Volume [ml/l]','Oxygen:Dissolved:CTD:Mass [µmol/kg]','Fluorescence:CTD:Seapoint','PAR:CTD [µE/m^2/sec]']
+    data = [['2015-01-0010.ctd',2.0,'11.1569','32.0634','24.4881','58.9','6.29','274.1','0.625','376.4']]
+    testing_df_set4 = pd.DataFrame(data, columns=column_names)
+
+    expected_params_set4 =[                             
+    {'whpname' : 'CTDPRS' , 'longname':'Pressure:CTD [dbar]', 'units' : 'DBAR'},
+    {'whpname' : 'CTDTMP' , 'longname':'Temperature:CTD [deg_C_(ITS90)]', 'units' : 'ITS-90'},
+    {'whpname' : 'CTDSAL' , 'longname':'Salinity:CTD [PSS-78]', 'units' : 'PSS-78'},
+    {'whpname' : 'CTDOXY' , 'longname':'Oxygen:Dissolved:CTD:Mass [µmol/kg]', 'units' : 'UMOL/KG'},   
+    {'whpname' : 'CTDBEAMCP' , 'longname':'Transmissivity:CTD [*/m]', 'units' : '/METER'},
+    {'whpname' : 'CTDFLUOR', 'longname': 'Fluorescence:CTD:Seapoint', 'units' : 'MG/M^3'},
+    {'whpname' : 'PAR' , 'longname':'PAR:CTD [µE/m^2/sec]', 'units' : 'UE/m^2/sec'}     
+    ] 
+
+
+    column_names = ['File Name','Pressure:CTD [dbar]','Temperature:CTD [deg_C_(ITS90)]','Salinity:CTD [PSS-78]','Sigma-t:CTD [kg/m^3]','Transmissivity:CTD [*/m]','Oxygen:Dissolved:CTD:Volume [ml/l]','Oxygen:Dissolved:CTD:Mass [µmol/kg]','Fluorescence:CTD:Seapoint [mg/m^3]','Fluorescence:CTD:Wetlabs [mg/m^3]','PAR:CTD [µE/m^2/sec]']
+    data = [['2017-01-0016.ctd',1.3,'9.1383','32.541','25.1974','','','','0.748','','0.1']]
+    testing_df_set5 = pd.DataFrame(data, columns=column_names)
+
+    expected_params_set5 =[                             
+    {'whpname' : 'CTDPRS' , 'longname':'Pressure:CTD [dbar]', 'units' : 'DBAR'},
+    {'whpname' : 'CTDTMP' , 'longname':'Temperature:CTD [deg_C_(ITS90)]', 'units' : 'ITS-90'},
+    {'whpname' : 'CTDSAL' , 'longname':'Salinity:CTD [PSS-78]', 'units' : 'PSS-78'},
+    {'whpname' : 'CTDOXY' , 'longname':'Oxygen:Dissolved:CTD:Mass [µmol/kg]', 'units' : 'UMOL/KG'},   
+    {'whpname' : 'CTDBEAMCP' , 'longname':'Transmissivity:CTD [*/m]', 'units' : '/METER'},
+    {'whpname' : 'CTDFLUOR' , 'longname':'Fluorescence:CTD:Seapoint [mg/m^3]', 'units' : 'MG/M^3'},   
+    {'whpname' : 'CTDFLUOR_TSG' , 'longname':'Fluorescence:CTD:Wetlabs [mg/m^3]', 'units' : 'MG/M^3'},
+    {'whpname' : 'PAR' , 'longname':'PAR:CTD [µE/m^2/sec]', 'units' : 'UE/m^2/sec'}     
+    ] 
+
+
+    testing_df_sets = (testing_df_set1, testing_df_set2, testing_df_set3, testing_df_set4, testing_df_set5)
+
+    expected_param_sets = (expected_params_set1, expected_params_set2, expected_params_set3, expected_params_set4, expected_params_set5)
+
+    return testing_df_sets, expected_param_sets
+
+
+def test_get_data_params(df_column_name_sets):
+
+    # For each variation of column names, confirm the 
+    # mapping of their long names to WHP names
+
+    testing_df_sets, expected_param_sets = df_column_name_sets
+
+    #real_params = get_data_params(testing_df_sets[0])
+
+    for expected_params, testing_df in zip(expected_param_sets, testing_df_sets):
+
+        real_params = get_data_params(testing_df)
+
+        assert real_params == expected_params
+
+
+def test_get_data_units():
+
+    data_params =[                             
+    {'whpname' : 'CTDPRS' , 'longname':'Pressure:CTD [dbar]', 'units' : 'DBAR'},
+    {'whpname' : 'CTDTMP' , 'longname':'Temperature:CTD [deg_C_(ITS90)]', 'units' : 'ITS-90'},
+    {'whpname' : 'CTDSAL' , 'longname':'Salinity:CTD [PSS-78]', 'units' : 'PSS-78'},
+    {'whpname' : 'CTDOXY' , 'longname':'Oxygen:Dissolved:CTD:Mass [µmol/kg]', 'units' : 'UMOL/KG'},   
+    {'whpname' : 'CTDBEAMCP' , 'longname':'Transmissivity:CTD [*/m]', 'units' : '/METER'},
+    {'whpname' : 'CTDFLUOR' , 'longname':'Fluorescence:CTD:Seapoint [mg/m^3]', 'units' : 'MG/M^3'},   
+    {'whpname' : 'PAR' , 'longname':'PAR:CTD [µE/m^2/sec]', 'units' : 'UE/m^2/sec'}     
+    ] 
+
+    expected_data_units = {'CTDPRS': 'DBAR', 'CTDTMP': 'ITS-90', 'CTDSAL': 'PSS-78', 'CTDOXY': 'UMOL/KG', 'CTDBEAMCP': '/METER', 'CTDFLUOR': 'MG/M^3', 'PAR': 'UE/m^2/sec'} 
+
+
+    real_data_units = get_data_units(data_params)
+
+    assert real_data_units == expected_data_units
+
